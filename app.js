c7fa82d3be740c806093c39444f94e139a86312b
Andrew Clark <mrandyclark@gmail.com>
2014-12-04 10:59:23 -0700

    move mongoose connection above routes, per pete

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/projectsteam');
+
 var routes = require('./routes');
 app.use('/', routes);
 
@@ -54,10 +57,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/projectsteam');
 
 module.exports = app;
 
