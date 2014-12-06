158a5c7db55016c5e21fcd0ffdf6582a2a40cf9e
Andrew Clark <mrandyclark@gmail.com>
2014-12-06 15:04:25 -0700

    all layouts added and complete.

---
+++ public/javascripts/project-team-app.js
@@ -0,0 +1,17 @@
+// global js
+ProjectTeamApp = function () { };
+
+ProjectTeamApp.prototype.init = function () {
+	// events
+	this.bindEvents();
+};
+
+ProjectTeamApp.prototype.bindEvents = function() {
+
+	// $(".editable-content").on(
+	// 	"click",
+	// 	$.proxy(this.handleEditableContent, this)
+	// );
+};
+
+ProjectTeamApp.prototype.handleEditableContent = function(evt) { };
--- routes/index.js
+++ routes/index.js
@@ -10,130 +10,127 @@ var db = require('../db');
 var express = require('express');
 var router = express.Router();
 
-// home page
-router.get('/', function(req, res) {
-	// res.render('index', { title: 'Express' });
-	res.render('index');
-});
+// gets replaced by a db call
+function getValue(id, data) {
+	for(var i = 0; i < data.length; i++) {
+		if(data[i].id == id)
+		{
+			return data[i];
+		}	
+	}
+	return null;
+}
 
+var allRoutes = {
+	'/': function(req, res) {
+		res.render('index');
+	},
 
-////////////////////
-/// code-reviews ///
-////////////////////
-router.get('/code-reviews', function (req, res) {
-	res.render('code-reviews/index');
-});
+	'/clients': function (req, res) {
+		res.render('clients/list', { clients: db.clients });
+	},
 
-router.get('/code-reviews/settings', function(req, res) {
-	var cantPresent = db.cantPresent;
-	var cantPresentMembers = [];
-	_.each(cantPresent, function(value) {
-		cantPresentMembers.push(
-			getValue(value, db.members)
-		)
-	});
+	'/projects': function (req, res) {
+		res.render('projects/list', { projects: db.projects });
+	},
 
-	console.log(cantPresentMembers);
+	'/code-reviews': function (req, res) {
+		res.render('code-reviews/index');
+	},
 
-	res.render(
-		'code-reviews/settings',
-		{	
-			rooms: db.reviewRooms,
-			cantPresent: cantPresentMembers 
-		}
-	)
+	'/code-reviews/settings': function(req, res) {
+		var cantPresent = db.cantPresent;
+		var cantPresentMembers = [];
+		_.each(cantPresent, function(value) {
+			cantPresentMembers.push(
+				getValue(value, db.members)
+			)
+		});
+		
+		res.render(
+			'code-reviews/settings',
+			{	
+				rooms: db.reviewRooms,
+				cantPresent: cantPresentMembers 
+			}
+		);
+	},
 
-	function getValue(id, data) {
-		for(var i = 0; i < data.length; i++) {
-			if(data[i].id == id)
-			{
-				return data[i];
-			}	
-		}
-		return null;
-	};
-});
+	'/tech-focus': function(req, res) {
+		res.render('tech-focus/index');
+	},
 
-////////////////////////
-/// current schedule ///
-////////////////////////
-router.get('/current-schedule', function (req, res) {
+	// list
+	'/members': function (req, res) {
+		res.render('members/list', { members: db.members });
+	},
 
-	allDates = [];
+	// ability to pass:
+	// /scheudle/members 		--> shows all members (default)
+	// /scheudle/projects 		--> shows all projects
+	// /schedule/member/:id 	--> shows specific member scheudle
+	// /scheudle/project/:id 	--> shows specific project schedule
+	// /schedule/client/:id 	--> shows multiple projects (all projects for a client)
+	'/schedule': function (req, res) {
+		allDates = [];
 
-	var currentDate = moment("2014-11-03");
-	this.allDates.push(
-		currentDate.format("MM/DD")
-	);
-
-	// figure out how we want to handle max-date
-	for(var i = 0; i < 30; i++) {
-		currentDate.add(7, 'days')
+		var currentDate = moment("2014-11-03");
 		this.allDates.push(
 			currentDate.format("MM/DD")
 		);
-	}
 
-	res.render(
-		'current-schedule/index',
-		{
-			dates: allDates,
-			members: db.members
+		// figure out how we want to handle max-date
+		for(var i = 0; i < 30; i++) {
+			currentDate.add(7, 'days')
+			this.allDates.push(
+				currentDate.format("MM/DD")
+			);
 		}
-	);
-});
 
-///////////////
-/// members ///
-///////////////
-
-// list
-router.get('/members', function (req, res) {
-	res.render('members/list', { members: db.members });
-});
-
-// read
-router.get('/member/:id', function (req, res) {
-	var id = req.params.id;
-	var member = getValue(id, db.members);
-	var position = getValue(member.position, db.positions);
-	var reportsTo = getValue(member.reportsTo, db.members)
-
-	res.render(
-		'members/view',
-		{
-			member: member,
-			position: position,
-			reportsTo: reportsTo
-		}
-	);
-
-	function getValue(id, data) {
-		for(var i = 0; i < data.length; i++) {
-			if(data[i].id == id)
+		res.render(
+			'schedule/index',
 			{
-				return data[i];
-			}	
-		}
-		return null;
-	};
+				dates: allDates,
+				members: db.members
+			}
+		);
+	},
+
+	'/member/:id': function (req, res) {
+		var id = req.params.id;
+		var member = getValue(id, db.members);
+		var position = getValue(member.position, db.positions);
+		var reportsTo = getValue(member.reportsTo, db.members)
+
+		res.render(
+			'members/view',
+			{
+				member: member,
+				position: position,
+				reportsTo: reportsTo
+			}
+		);
+	}
+};
+
+_.each(allRoutes, function(value, key) {
+	router.get(key, value);
 });
-
-// create
-router.post('/member', function (req, res) {
-	res.send('Got a POST request at /member');
-});
-
-// update
-router.put('/member', function (req, res) {
-	res.send('Got a PUT request at /member');
-});
-
-// destroy
-router.delete('/member', function (req, res) {
-	res.send('Got a DELETE request at /member');
-});
-
-
 
 module.exports = router;
+
+
+// // create
+// router.post('/member', function (req, res) {
+// 	res.send('Got a POST request at /member');
+// });
+
+// // update
+// router.put('/member', function (req, res) {
+// 	res.send('Got a PUT request at /member');
+// });
+
+// // destroy
+// router.delete('/member', function (req, res) {
+// 	res.send('Got a DELETE request at /member');
+// });
